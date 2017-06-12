3c84bc14ae8f03c7e05e79a7147dd5d759e8332e
maruf10155 <maruf10155@users.noreply.github.com>
2017-06-12 17:56:48 -0400

    Create cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,30 +1,30 @@
 
 ## Creating a list
-makeCacheMatrix=function(x=matrix()){
- inverse=NULL     
- set=function(y){
-   x<<-y       ## set the value of matrix         
-   inverse<<-NULL
+makeCacheMatrix = function (x = matrix () ) {
+ inverse = NULL     
+ set = function ( y ) {
+   x <<- y       ## set the value of matrix         
+   inverse <<- NULL
  }
- get=function()x ##get the value of matrix
- setinverse=function(inverse) inverse<<-inverse ##set inverse of matrix
- getinverse=function() inverse ##get inverse of matrix
- list(set=set,get=get,setinverse=setinverse,
-      getinverse=getinverse) ## create a list of functions
+ get = function () x   ##get the value of matrix
+ setinverse = function ( inverse )  inverse <<- inverse ##set inverse of matrix
+ getinverse = function () inverse  ##get inverse of matrix
+ list ( set = set , get = get , setinverse = setinverse,
+      getinverse = getinverse ) ## create a list of functions
 }
 
 
 
 ## this function gives inverse of matrix or gives a cached data
 
-cacheSolve=function(x,...){
-  inv=x$getinverse()
-  if(!is.null(inv)){
-    message("getting cached data")
-    return(inv)
+cacheSolve = function ( x,...) {
+  inv = x$getinverse ()
+  if ( !is.null ( inv )) {
+    message ( "getting cached data" )
+    return ( inv )
   }
-  mat=x$get()
-  inv=solve(mat,...)
-  x$setinverse(inv)
+  mat = x$get ()
+  inv = solve ( mat,... )
+  x$setinverse( inv )
   inv
 }
